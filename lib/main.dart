e9ddbc9932c34cb14a8406400e05246c8b57554e
Your Name~manojprabhakar24 <manojptn16@gmail.com>
2023-09-29 10:42:54 +0530

    fetch and displayed the data

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart'as http;
+import 'dart:convert';
+void main() {
+  runApp(MyApp());
+}
+
+
+
+Future<Album> fetchAlbum()async{
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/todos/1'));
+  if (response.statusCode == 200){
+    return Album.fromJson(jsonDecode(response.body));
+  }else{
+    throw Exception("Simply Waste");
+  }
+}
+
+class Album {
+  final int userId;
+  final int id;
+  final String title;
+
+  Album({required this.userId, required this.id, required this.title});
+
+  factory Album.fromJson(Map<String, dynamic>json){
+    return Album(userId: json['userId'], id: json['id'], title: json['title']);
+  }
+}
+
+
+
+
+class MyApp extends StatefulWidget{
+  @override
+  State<MyApp> createState()=> _MyAppState();
+
+  }
+
+class _MyAppState extends State<MyApp>{
+  late Future<Album> futureAlbum;
+  @override
+  void initState(){
+    super.initState();
+    futureAlbum=fetchAlbum();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Fetched Data'),
+        ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context,snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.title);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
+
+}
+
+
+
+
+
+
+
