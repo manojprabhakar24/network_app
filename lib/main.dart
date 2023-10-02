1eee85e375afb3c8dacdbbc387a20e9f05cb95e1
Your Name~manojprabhakar24 <manojptn16@gmail.com>
2023-10-02 16:43:13 +0530

    display the fetched data from internet

--- lib/main.dart
+++ lib/main.dart
@@ -1,56 +1,92 @@
-import 'package:flutter/material.dart';
+
 import 'dart:convert';
 
-import 'package:http/http.dart';
-void main() {
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main(){
   runApp(MyApp());
 }
 
+Future<Album> fetchAlbum() async {
+  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users/6'));
+  if (response.statusCode == 200) {
+    return Album.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('Failed to load album');
+  }
+}
+
+
+class Album {
+  final String username;
+  final String name;
+  final String email;
+
+  Album( {
+    required this.name,
+    required this.username,
+    required this.email,
+  });
+
+  factory Album.fromJson(Map<String,dynamic> json) {
+    return Album(
+      name: json['name'],
+      username: json['username'],
+      email: json['email'],
+    );
+  }
+}
 class MyApp extends StatefulWidget {
 
-
   @override
-  _MyAppState createState() => _MyAppState();
+  State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
 
-  final url="https://jsonplaceholder.typicode.com/posts";
-  var _Mydata = [];
-  void MyPost()async{
-    try {
-      final response = await get(Uri.parse(url));
-      final MyData = jsonDecode(response.body) ;
-      setState(() {
-        _Mydata=MyData;
-      });
-    }catch(err) {}
-  }
+  late Future<Album> futureAlbum;
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    MyPost();
+    futureAlbum = fetchAlbum();
   }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
       home: Scaffold(
         appBar: AppBar(
-         title: Text("Network Fetched")
+          title:Text('Update Data'),
         ),
-        body: ListView.builder(
-            itemCount: _Mydata.length,
-            itemBuilder: (context,index){
-              final display=_Mydata[index];
-              return Text("Title:${display["title"]}\n"
-                    "Body:${display["body"]}\n");
-            }
-    ),
+        body: Center(
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState==ConnectionState.done) {
+                if (snapshot.hasData) {
+                  return Column(
+
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(snapshot.data!.name),
+                      Text(snapshot.data!.username),
+                      Text(snapshot.data!.email),
+
+                    ],
+                  );
+                } else if (snapshot.hasError) {
+                  return Text('${snapshot.error}');
+                }
+              }
+              // By default, show a loading spinner.
+              return  CircularProgressIndicator();
+            },
+          ),
+        ),
       ),
     );
   }
 }
-
